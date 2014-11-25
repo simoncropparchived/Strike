0624d67e3e7cfe89a9a8434422e195c47cee466a
Simon Cropp <simon.cropp@gmail.com>
2014-11-25 18:20:21 +1100

    version

--- Common/AssemblyInfo.cs
+++ Common/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Strike")]
 [assembly: AssemblyProduct("Strike")]
-[assembly: AssemblyVersion("0.9.1")]
-[assembly: AssemblyFileVersion("0.9.1")]
+[assembly: AssemblyVersion("0.10.0")]
+[assembly: AssemblyFileVersion("0.10.0")]
