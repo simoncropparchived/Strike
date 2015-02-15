847187ba2e041e1b59b2ceffcb4ac6f907aed180
Simon Cropp <simon.cropp@gmail.com>
2015-02-15 23:07:37 +1100

    refs

--- Common/AssemblyInfo.cs
+++ Common/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Strike")]
 [assembly: AssemblyProduct("Strike")]
-[assembly: AssemblyVersion("0.10.0")]
-[assembly: AssemblyFileVersion("0.10.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
